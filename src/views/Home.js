8e121c384aa3c6470903fe13cbdbe9b3efa2c2ff
Majstruk Guillaume <guillaume@Majstruks-MacBook-Pro.local>
2021-07-15 16:26:01 +0200

    fixed home w/ grid container

--- src/views/Home.js
+++ src/views/Home.js
@@ -15,31 +15,28 @@ const Home = () => {
                     <h3 className="text-normal">27 yo</h3>
                     <h2 className="mt-sm">FULLSTACK DEVELOPER</h2>
                     <h3 className="underlined font-space">OPEN TO WORK</h3>
-                    <div className="row mt-sm">
-                        <div className="column mr-md">
-                            <div className="column">
-                                <h4>42 School</h4>
-                                <span>Junior</span>
-                            </div>
-                            <div className="column mt-xs">
-                                <h4>Aeronautics Mecanic Graduate</h4>
-                                <span>Very Good</span>
-                            </div>
-                            <div className="column mt-xs">
-                                <h4>Scientific Graduation</h4>
-                                <span>Well</span>
-                            </div>
+                    <div id="home-grid" className="grid-wrapper mt-lg w-100">
+                        <div>
+                            <div className="text-bold">42, Paris</div>                            
+                            <div>Junior</div>
+                            <br></br>
+                            <div className="text-bold">Aeronautics Mechanics Graduation</div>                            
+                            <div>Very Good</div>
+                            <br></br>
+                            <div className="text-bold">Scientific Graduation</div>                            
+                            <div>Well</div>
+                            <br></br>
                         </div>
-                        <div className="column">
-                            <div className="row">
+                        <div>
+                            <div>
                                 <Icon icon={faMapMarkerAlt} className="mr-xs"></Icon>
                                 Chantilly, Oise, France
                             </div>
-                            <div className="row mt-xs">
+                            <div className="mt-xs">
                                 <Icon icon={faGithub} className="mr-xs"></Icon>
                                 <a style={{color: "var(--link-color)"}}href="https://www.google.com" rel="noreferrer" target="_blank">/GuillaumeMajstruk</a>
                             </div>
-                            <div className="row mt-xs">
+                            <div className="mt-xs">
                                 <Icon icon={faLinkedin} className="mr-xs"></Icon>
                                 <a style={{color: "var(--link-color)"}}href="https://www.google.com" rel="noreferrer" target="_blank">/in/guillaume-majstruk</a>
                             </div>
