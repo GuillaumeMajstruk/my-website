460605abeb99acac2f501009bb844f61d55c86bb
Majstruk Guillaume <guillaume@Majstruks-MacBook-Pro.local>
2021-07-25 12:21:35 +0200

    production without experience (wip)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
 import Nav from './components/navbar';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+require('dotenv').config();
 
 function App() {
   return (
     <div className="container">
+      <ToastContainer position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover/>
       <Nav />
     </div>
   );
--- src/views/Contact.js
+++ src/views/Contact.js
@@ -1,36 +1,89 @@
 import {FontAwesomeIcon as Icon} from '@fortawesome/react-fontawesome';
 import {faHourglassHalf} from '@fortawesome/free-solid-svg-icons';
+import emailjs from 'emailjs-com';
+import { useForm } from 'react-hook-form';
+import { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
 
 const Contact = () => {
+    const { register, handleSubmit, formState, reset} = useForm();
+    const {errors, isSubmitSuccessful} = formState;
+
+    const [isLoading, setLoading] = useState(false);
+    useEffect(() => {
+        if (isSubmitSuccessful) {
+            reset({
+                first_name: '',
+                last_name: '',
+                from_email: '',
+                subject: '',
+                message: ''
+            });
+        }
+    }, [formState, isSubmitSuccessful, reset]);
+
+    const sendMsg = async () => {
+        try {
+            setLoading(true);
+            await emailjs.sendForm(
+                process.env.REACT_APP_MAIL_SERVICE,
+                process.env.REACT_APP_MAIL_TEMPLATE,
+                "#contact-col",
+                process.env.REACT_APP_MAIL_USER
+            );
+            toast.success("Your message were succesfully sent.");
+        } catch (error) {
+            console.log("ERROR", error);
+            toast.error("An error occured sending your message...")
+        }
+        setLoading(false);
+        return;
+    }
+
     return (
         <div className="column container fullscreen center-h center-v">
             <div className="background-header">Contact</div>
             <div className="center-h h-100 ml-auto mr-auto" style={{width: "90%"}}>
-                <div id="contact-col" className="column h-100 center-v ml-auto mr-auto gap">
+                <form id="contact-col" className="column h-100 center-v ml-auto mr-auto gap" onSubmit={handleSubmit(sendMsg)}>
                     <h2 className="font-rhyme">ASK ME ABOUT EVERYTHING YOU WANT</h2>
                     <h3 className="font-space text-normal mt-xs mb-sm">I'll be responding you ASAP <Icon icon={faHourglassHalf}></Icon></h3>
                     <div className="gap" style={{
                         display: "flex",
                         flexFlow: "row wrap"
                     }}>
-                        <input type="text" placeholder="firstname..."></input>
-                        <input type="text" placeholder="lastname..."></input>
+                        <div className="input-layout">
+                            <input type="text" placeholder="FIRST NAME..." name="first_name" {...register("first_name", {required: true})}></input>
+                            {errors.first_name?.type === 'required' && <div className="input-error">First name is required</div>}
+                        </div>
+                        <div className="input-layout">
+                            <input type="text" placeholder="LAST NAME..." name="last_name" {...register("last_name", {required: true})}></input>
+                            {errors.last_name?.type === 'required' && <div className="input-error">Last name is required</div>}
+                        </div>
                     </div>
                     <div className="gap" style={{
                         display: "flex",
                         flexFlow: "row wrap"
                     }}>
-                        <input type="text" placeholder="email..."></input>
-                        <input type="text" placeholder="subject..."></input>
+                        <div className="input-layout">
+                            <input type="text" placeholder="MAIL..." name="from_email" {...register("from_email", {required: true})}></input>
+                            {errors.from_email?.type === 'required' &&  <div className="input-error">Mail is required</div>}
+                        </div>
+                        <div className="input-layout">
+                            <input type="text" placeholder="SUBJECT..." name="subject" {...register("subject", {required: true})}></input>
+                            {errors.subject?.type === 'required' && <div className="input-error">Subject is required</div>}
+                        </div>
                     </div>
                     <div className="gap" style={{
                         display: "flex",
                         flexFlow: "row wrap"
                     }}>
-                        <textarea placeholder="your message"></textarea>
+                        <div className="input-layout">
+                            <textarea placeholder="YOUR MESSAGE..." name="message" {...register("message", {required: true})}></textarea>
+                            {errors.message?.type === 'required' && <div className="input-error">Message is required</div>}
+                        </div >
                     </div>
-                    <button className="button">Send</button>
-                </div>
+                    <button disabled={isLoading} className="button" type="submit">Send</button>
+                </form>
             </div>
         </div>
     )
