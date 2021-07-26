42a9735124fd48ff8aaf5148d6d851a9e95275e7
Majstruk Guillaume <guillaume@Majstruks-MacBook-Pro.local>
2021-07-26 02:09:11 +0200

    fix -> grid alignment in hobbies grid / removed template style in experience view

--- src/views/Experience.js
+++ src/views/Experience.js
@@ -89,7 +89,7 @@ const Experience = () => {
     return (
         <div className="column container fullscreen center-h center-v">
             <div className="background-header">xp</div>
-            <div className="center-h h-100 ml-auto mr-auto" style={{width: "90%"}}>
+            <div className="center-h h-100 ml-auto mr-auto">
                 <div className="column h-100 center-v">
                     <h2 className="font-rhyme text-light mt-sm">WHAT ABOUT MY <strong>CAREER?</strong>&nbsp;<Icon icon={faUserTie}></Icon></h2>
                     <span className="font-normal">Here is a timeline of it</span>
