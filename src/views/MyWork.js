1916f82d1528378a4aecd8123bc2c56cab51f60f
Majstruk Guillaume <guillaume.majstruk@hotmail.fr>
2021-08-21 13:12:30 +0200

    creating mywork view

---
+++ src/views/MyWork.js
@@ -0,0 +1,13 @@
+const Hobbies = () => {
+        return (
+        <div className="column container fullscreen center-h center-v">
+            <div id="hobbies-container" className="center-h h-100 ml-auto mr-auto">
+                <div className="column h-100 center-v">
+                    <h1>This page is under construction</h1> :
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Hobbies;
