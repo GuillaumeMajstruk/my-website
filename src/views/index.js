5614ed1b986628c001754e62856118a2403a544a
Majstruk Guillaume <guillaume.majstruk@hotmail.fr>
2021-08-21 13:12:03 +0200

    created module for views

---
+++ src/views/index.js
@@ -0,0 +1,7 @@
+export { default as Contact } from "./Contact";
+export { default as Experience } from "./Experience";
+export { default as Hobbies } from "./Hobbies";
+export { default as Home } from "./Home";
+export { default as MyWork } from "./MyWork";
+export { default as NotFound } from "./NotFound";
+export { default as Skills } from "./Skills";
